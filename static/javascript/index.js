e02bcfeb76f0e2c541e4ab0dc2d5adce831575d2
Cecilia <ceciliataboada1@gmail.com>
2023-07-19 21:46:38 -0300

    creating connections with API

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -4,3 +4,23 @@ function hideShow(idToShow, idToHide){
     document.getElementById(idToHide).style.display = "none"
 }
 
+const newUserForm = document.querySelector("#signupform")
+
+newUserForm.addEventListener("submit", async e => {
+    const email = newUserForm["email"].value
+    const password = newUserForm["password"].value
+    const name = newUserForm["name"].value
+
+    const response = await fetch("/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email,
+            password,
+            name
+        })
+    })
+    const data = await response.json()
+})
